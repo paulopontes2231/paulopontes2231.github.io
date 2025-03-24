c56297abbe82611de4f00c6a6c13982546f18ac7
1181601 <1181601@isep.ipp.pt>
2025-03-24 22:25:54 +0000

    teste

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [{ path: 'isItMonday', component: IsItMondayComponent}, {
                        ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
