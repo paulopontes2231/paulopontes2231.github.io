e0bef7d693e1693119fc17e697e52fe4655c40ca
1181601 <1181601@isep.ipp.pt>
2025-03-24 18:57:32 +0000

    a

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,9 +1,41 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, animate, transition, group } from '@angular/animations';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.scss']
+  styleUrls: ['./landing.component.scss'],
+  animations: [
+
+
+    trigger('slideInRight', [
+      transition(':enter', [
+        style({ transform: 'translateX(100%)' }),
+        animate('1000ms ease-out', style({ transform: 'translateX(0)' })),
+      ]),
+      transition(":leave", [
+        animate('1000ms ease-out', style({ transform: 'translateX(100%)' })),
+      ])
+    ]),
+    trigger('slideInLeft', [
+      transition(':enter', [
+        style({ transform: 'translateX(-100%)' }), // Start off-screen to the left
+        animate('1000ms ease-out', style({ transform: 'translateX(0)' })), // Animate to normal position
+      ]),
+      transition(':leave', [
+        animate('1000ms ease-out', style({ transform: 'translateX(-100%)' })), // Animate back to the left
+      ])
+    ]),
+    trigger('showState', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('2s', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        animate('2s', style({ opacity: 0 }))
+      ])
+    ]),
+  ]
 })
 export class LandingComponent {
 
