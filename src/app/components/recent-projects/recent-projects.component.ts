5172fced96cc5a93ab7e0834690aa35816dd9334
paulopontes2231 <paulopontes2231@gmail.com>
2025-09-22 18:47:08 +0100

    Remove CDP torneio from projects

--- src/app/components/recent-projects/recent-projects.component.ts
+++ src/app/components/recent-projects/recent-projects.component.ts
@@ -14,8 +14,8 @@ export class RecentProjectsComponent {
       new Project('assets/icons/sudokuSolver.PNG', "Sudoku Solver", "An Angular and Golang app that solves Sudoku puzzles using a backtracking algorithm.", '#/sudoku-solver', true),
       new Project('assets/icons/gameOfLife.PNG', "Game of Life", "An Angular app that simulates the Game of Life, a cellular automaton.", '#/game-of-life', true),
       new Project('assets/icons/whatsAppBot.png', "WhatsApp Bot", "A Node.js app that sends a message at a specific time of day.\nNot available right now, only upon request.", '#/game-of-life', false),
-      new Project('assets/icons/cdp24horas.PNG', "CDP 24 Horas", "An Angular and Node.js app which displays the results of a 24-hour tournament.", 'https://cdp24horas.eu', true),
-      new Project('assets/icons/joker.png', "Joker", "An Angular and Golang app that recreates the popular portuguese TV show. Beta version - Still needs to be polished.", '#/joker', true),
+/*       new Project('assets/icons/cdp24horas.PNG', "CDP 24 Horas", "An Angular and Node.js app which displays the results of a 24-hour tournament.", 'https://cdp24horas.eu', true),
+ */      new Project('assets/icons/joker.png', "Joker", "An Angular and Golang app that recreates the popular portuguese TV show. Beta version - Still needs to be polished.", '#/joker', true),
     ];
 
 }
